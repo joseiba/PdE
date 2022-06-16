303d96ddeb7e8803d08c7e5378d05edfd95af83a
joseiba <familiajoselo@gmail.com>
2022-06-15 23:32:45 -0400

    primer commit

--- PdE/conf/settings.py
+++ PdE/conf/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#ja!q#gh90apoww)&ao_aykh*2466(zh!+q-=uowi(z!!u(8_(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
